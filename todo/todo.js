85c91327abf2cd332c9aac51c773b57f6062b327
Bryce Romney <b-romney@kellogg.northwestern.edu>
2017-02-01 00:21:02 -0600

    update

--- todo/todo.js
+++ todo/todo.js
@@ -1,9 +1,28 @@
 // This code runs when the page loads
 $(function() {
 
-  $(".btn").on("click", function(event) {
+  $("#1a").on("click", function(event) {
     event.preventDefault()
     $("#1").hide()
   });
-
+  $("#2a").on("click", function(event) {
+    event.preventDefault()
+    $("#2").hide()
+  });
+  $("#3a").on("click", function(event) {
+    event.preventDefault()
+    $("#3").hide()
+  });
+  $("#4a").on("click", function(event) {
+    event.preventDefault()
+    $("#4").hide()
+  });
+  $("#5a").on("click", function(event) {
+    event.preventDefault()
+    $("#5").hide()
+  });
+  $("#6a").on("click", function(event) {
+    event.preventDefault()
+    $("#6").hide()
+  });
 })
